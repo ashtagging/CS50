ab7e834ed9eddae3150679042eb501b5fa04c390
Ashley Littlewood <ashley.littlewood22@gmail.com>
2023-03-10 21:39:07 +0000

    problem set 1 - mario

---
+++ Week 1/mario_more.c
@@ -0,0 +1,33 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    do{
+        n = get_int("Please enter your desired block height between 1 & 8: ");
+    } while(n < 1 || n > 8);
+
+    for(int i=0; i< n; i++)
+    {
+        for(int j=0; j < n; j++)
+        {
+          if((i + j) < (n-1))
+          {
+             printf(" ");
+          } else{
+            printf("#");
+          }
+        }
+        printf("  ");
+        for(int k=0; k<n; k++)
+        {
+          if(k <= i)
+          {
+             printf("#");
+          }
+        }
+        printf("\n");
+    }
+}
